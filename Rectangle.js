856aa778a4f1604a95a68e586dbe4351d0742876
Xabe04-14 <be.y26@student.passerellesnumeriques.org>
2024-09-09 21:52:02 +0700

    Be commit

---
+++ Rectangle.js
@@ -0,0 +1,15 @@
+var width=prompt("Mời bạn nhập chiều dài hcn: ");
+var height= prompt("Mời bạn nhập chiều rộng hcn: ");
+var area=width*height;
+
+var status=confirm("Do you want to continue?");
+if (status==True){
+    ("https://www.google.com/");}
+else{
+    ("https://fafacebook.com/");
+}
+
+
+document.write(area);
+alert(a);
+// document.getElementById('kq').innerHTML=area
---
+++ script.js
@@ -0,0 +1,8 @@
+
+        var a= "Hello";
+        var b= "World!";
+        var c=a+""+b;
+        // alert(c); Hiển thị ra hộp thông báo
+        // document.write(c) hiển thị ra dòng p và kết quả
+        //document.getElementById(đưa con trỏ chuột tới đại chỉ id cần sd)('kq').innerHTML=c; chỉ hiện thị ra kết quả mà không hiển thị ra dòng p
+        document.getElementById('kq').innerHTML=c; 
